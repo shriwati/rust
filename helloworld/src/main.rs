37be19bb7eb4407e121c511fdfb702dc603c65e6
Shri Sakrikar <shrisakrikar@hotmail.com>
2023-05-02 08:09:04 -0400

    read string and convert to number

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -1,4 +1,15 @@
-fn main() {
+use std::io;
+use std::io::Read;
+
+fn main(){
+
+    let mut number = String::new();
+
+    io::stdin().read_line(&mut number).expect("empty number");
+
+    let number:i32 =number.trim().parse().expect("not a number");
+
+    println!("number was {number}");
 
 
 }
