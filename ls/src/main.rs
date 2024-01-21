ab7d7b3a3d7e241be6793b2484dd95e87c537e33
shrisakrikar <shrisakrikar@hotmail.com>
2024-01-20 23:04:36 -0500

    Code complete. Lists all directories immediately under passed in directory.

--- ls/src/main.rs
+++ ls/src/main.rs
@@ -1,3 +1,24 @@
+use std::env::args;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let dir_name = args().skip(1).take(1);
+    for name in dir_name {
+        print_directories(&name);
+    }
+}
+
+fn print_directories(dir_name: &str) {
+    println!("listing directories under - {}", &dir_name);
+    if let Ok(entries) = fs::read_dir(dir_name) {
+        for entry in entries {
+            let dir = entry.unwrap();
+            let dir_type = dir.file_type().unwrap();
+            if dir_type.is_dir() {
+                println!("{:?}", dir.path());
+            }
+        }
+    } else {
+        println!("No directory found under {}", dir_name);
+    }
 }
