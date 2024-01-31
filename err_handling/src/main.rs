9bc9565317e9589ad1c906b11ece24111fc83e8d
shrisakrikar <shrisakrikar@hotmail.com>
2024-01-31 08:20:54 -0500

    Code completed for math errors,still looking for better solution than match.

---
+++ err_handling/src/main.rs
@@ -0,0 +1,18 @@
+// use std::fmt::Error;
+use std::io::Error;
+
+fn divide_by_zero(numerator: i32, denominator: i32) -> Result<f64, &'static str> {
+    if denominator == 0 {
+        return Err("Can not divid by zero");
+    }
+    Ok((numerator / denominator) as f64)
+}
+
+fn main() {
+    let result = divide_by_zero(100, 0);
+
+    match result {
+        Ok(result) => println!("Answer is {}", result),
+        Err(error) => println!("Error occurred {}", error),
+    }
+}
