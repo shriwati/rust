170d62613385a2fdfa57acf5422fcf0e9dd689e3
shrisakrikar <shrisakrikar@hotmail.com>
2024-03-19 07:32:23 -0400

    elevator completed.

--- gcd/src/main.rs
+++ gcd/src/main.rs
@@ -2,19 +2,23 @@ mod gcd;
 mod collatz_seq;
 mod matrix;
 mod magnitude;
+mod elevator;
 
+use std::cell::Cell;
+use crate::elevator::{Direction, Elevator};
 use crate::magnitude::{magnitude, normalize};
 
-fn incr(n:&mut i32){
-    *n +=1
-}
-
-fn get_first(name: &(String, String)) -> &String {
-    &name.0
-}
-
-
 fn main() {
+
+    // let i = Cell::new(10);
+    //
+    // println!("Cell is {}",i.get());
+    // i.set(15);
+    // println!("Cell is {}",i.get());
+    let mut ele = Elevator::start();
+    ele.move_elevator(Direction::Up,5);
+    ele.move_elevator(Direction::Down,4);
+    ele.elevator_is_on();
 
     println!("Magnitude of a unit vector: {}", magnitude(&vec![0.0, 1.0, 0.0]));
     let mut v = vec![1.0, 2.0, 9.0];
