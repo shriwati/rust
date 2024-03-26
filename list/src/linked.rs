1439f52d50141038605676242449f29f2ebacb07
shrisakrikar <shrisakrikar@hotmail.com>
2024-03-26 07:41:22 -0400

    2 links created. Node changed in to box.

--- list/src/linked.rs
+++ list/src/linked.rs
@@ -1,16 +1,18 @@
 pub mod linked_list {
 
-    #[derive(Debug)]
-    pub struct Node<T>{
-        pub element:Option<T>,
-        pub next:Option<Box<Node<T>>>
+    #[derive(Debug,  Clone)]
+    pub struct Node<T> {
+        pub element: Option<T>,
+        pub next: Option<Box<Node<T>>>,
     }
 
-    impl <T> Node<T>{
-        pub fn new(data:Option<T>) ->Node<T>{
-            Node{element:data,next: None } // create new node
+    impl <T> Node< T> {
+        pub fn new(data: Option<T>) -> Box<Node<T>> {
+            Box::new(Node
+            {
+                element: data,
+                next: None
+            })
         }
     }
-
-
 }
