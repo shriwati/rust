bb841410309fa5cc5b5d274e2af095b540df8d58
shrisakrikar <shrisakrikar@hotmail.com>
2024-04-15 14:37:56 -0400

    chat GPT version

--- list/src/linked.rs
+++ list/src/linked.rs
@@ -1,38 +1,42 @@
+use std::io::Empty;
 
-pub mod linked_list {
-    use std::fmt;
+// Define the Node struct
+struct Node<T> {
+    data: T,
+    next: Option<Box<Node<T>>>, // Use Option<Box<Node<T>>> to allow nodes to be nullable
+}
 
-    #[derive(Debug, Clone)]
-    pub struct Node<T> {
-        pub prev: Option<Box<Node<T>>>,
-        pub element: T,
-        pub next: Option<Box<Node<T>>>,
+// Define the LinkedList struct
+pub struct LinkedList<T> {
+    head: Option<Box<Node<T>>>, // Head of the linked list
+}
+
+// Implementation of methods for LinkedList
+impl<T> LinkedList<T> {
+    // Constructor
+    pub fn new() -> Self {
+        LinkedList { head: None }
     }
 
-
-    impl<T> Node<T> {
-        pub fn new(data: T) -> Option<Box<Node<T>>> {
-            Some(Box::new(Node{prev:None,element:data,next:None}))
-        }
-        pub fn add_next(&mut self, data: T) -> &Option<Box<Node<T>>> {
-            self.next = Node::new(data);
-            let x = &self.next;
-            x
-        }
+    // Method to insert a new node at the beginning of the linked list
+    pub fn push(&mut self, data: T) {
+        let new_node = Node {
+            data: data,
+            next: self.head.take(), // Take the current head and replace it with None
+        };
+        self.head = Some(Box::new(new_node)); // Wrap the new node in a Box and assign it as the new head
     }
 
-    // impl<T> fmt::Display for Node<T>
-    //     where
-    //         T: fmt::Display,
-    // {
-    //     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    //         let formatted_elements = self
-    //             .element
-    //             .iter()
-    //             .map(|el| format!("{}", el))
-    //             .collect::<Vec<_>>();
-    //         let elements = formatted_elements.join(", ");
-    //         write!(f, "[{:?}]", elements)
-    //     }
-    // }
+    // Method to remove the first node from the linked list and return its data
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next; // Move the next node to the head
+            node.data
+        })
+    }
+
+    // Method to check if the linked list is empty
+    pub fn is_empty(&self) -> bool {
+        self.head.is_none()
+    }
 }
