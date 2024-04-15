ab379a2ff0144cfd9aeb2719b9aaf05e5d0de9ba
shrisakrikar <shrisakrikar@hotmail.com>
2024-04-15 13:25:18 -0400

    temp check-in

---
+++ lists/src/lib.rs
@@ -0,0 +1,47 @@
+// pub mod first;
+
+use std::mem;
+
+pub struct List{
+    head:Link,
+}
+enum Link{
+    Empty,
+    More(Box<Node>)
+}
+
+struct Node{
+    element:i32,
+    next: Link
+}
+
+
+impl List{
+    pub fn new()->Self{
+        List{ head : Link::Empty} // return head that points to Empty Link
+    }
+
+    pub fn push(&mut self,data:i32){
+        let new_node=Box::new(Node{
+            element:data,
+            next: mem::replace(&mut self.head,Link::Empty) // temporarily replacing it with empty ptr
+        });
+        self.head = Link::More(new_node); // now point it to new node
+    }
+}
+
+//
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
