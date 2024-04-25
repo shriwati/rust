fa7c768e82ef6c73bcea9b66417b433be628afae
shrisakrikar <shrisakrikar@hotmail.com>
2024-04-24 23:37:59 -0400

    linked list with head and tail.

--- err_handling/src/main.rs
+++ err_handling/src/main.rs
@@ -1,6 +1,6 @@
 fn divide_by_zero(numerator: i32, denominator: i32) -> Result<f64, &'static str> {
     if denominator == 0 {
-        return Err("Can not divid by zero");
+        return Err("Can not divide by zero");
     }
     Ok((numerator / denominator) as f64)
 }
@@ -9,7 +9,7 @@ fn main() {
     let result = divide_by_zero(100, 0);
 
     match result {
-        Ok(result) => println!("Answer is {}", result),
-        Err(error) => println!("Error occurred {}", error),
+        Ok(result) => println!("Answer is :{}", result),
+        Err(error) => println!("Error occurred :{}", error),
     }
 }
