20dfea91923e084d54efbefb31e3076cae62ee02
shrisakrikar <shrisakrikar@hotmail.com>
2024-05-08 07:49:34 -0400

    typo fixed

--- err_handling/src/main.rs
+++ err_handling/src/main.rs
@@ -1,15 +1,19 @@
+use std::error::Error;
+
 fn divide_by_zero(numerator: i32, denominator: i32) -> Result<f64, &'static str> {
     if denominator == 0 {
-        return Err("Can not divid by zero");
+        return Err("Can not divide by zero");
     }
     Ok((numerator / denominator) as f64)
 }
 
 fn main() {
-    let result = divide_by_zero(100, 0);
+    let result = divide_by_zero(100, 10);
 
     match result {
         Ok(result) => println!("Answer is {}", result),
-        Err(error) => println!("Error occurred {}", error),
+        Err(error) => {
+            println!("Error occurred {},--source", error.source())
+        },
     }
 }
