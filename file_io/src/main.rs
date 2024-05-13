624d7b3d8379df7401e39e5d8de7428fbd2247f4
wati <shrisakrikar@hotmail.com>
2024-05-12 21:34:09 -0400

    all done. might do some refactoring later.

--- file_io/src/main.rs
+++ file_io/src/main.rs
@@ -1,31 +1,42 @@
-use std::{fs, io};
 use std::fs::File;
-use std::io::{Read, stdin};
+use std::io;
+use std::io::Read;
 
-#[derive(Debug)]
+use clap::Parser;
+
+mod cmd_args;
+
+
 struct FileInfo {
     name: String,
-    file_type: String,
 }
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short)]
+    f: String,
+    #[arg(short)]
+    t: String,
+}
 impl FileInfo {
-    pub fn new(name: String, file_type: String) -> Self {
-        FileInfo { name, file_type } // moved here
+    pub fn new(name: String) -> Self {
+        FileInfo { name } // moved here
     }
     fn open_file(file_name: &str) -> io::Result<File> {
-            File::open(&file_name)
+        File::open(&file_name)
     }
 
     pub fn read_text_file(&self) {
         println!("Reading file..{}", &self.name);
         let f = Self::open_file(&self.name);
-        let mut data=String::new();
+        let mut data = String::new();
         // read string
-        let _  = f.unwrap().read_to_string(&mut data);
-        println!("Contents of the file '{}' were {:?}",&self.name, &data);
+        let _ = f.unwrap().read_to_string(&mut data);
+        println!("Contents of the file '{}' were {:?}", &self.name, &data);
     }
 
-    pub fn read(&self) -> std::io::Result<()> {
+    pub fn read_bytes(&self) {
         // open the file for read only
         // read contents in to a vector
         // return readonly FileInfo
@@ -33,41 +44,21 @@ impl FileInfo {
         let f = Self::open_file(&self.name);
         // read bytes
         let mut data = Vec::new();
-        let _ = f?.read_to_end(&mut data);
-        println!("{:?}", &data);
-        Ok(())
+        let _ = f.unwrap().read_to_end(&mut data);
+        println!("Contents of the file '{}' were {:?}", &self.name, &data)
     }
 }
 
 fn main() {
+    let cli = Cli::parse();
 
-    println!("Please type :");
-    println!("1.Full path and file name to read");
-    println!("2.Read as b:Binary, t:Text");
+    let filename = cli.f;
+    let choice:&str  = &cli.t.to_ascii_lowercase();
+    let f = FileInfo::new(filename);
 
-    let mut line = &mut String::new();
-    let _  = stdin().read_line(&mut line);
-    loop {
-        if &line.len() > &(0 as usize) {
-            break;
-        } else {
-            line.clear();
-            let _ = stdin().read_line(&mut line);
-        }
-    }
-    println!("Input was {}", &line);
-
-    if line.len() > 1 {
-        let filename:String= line.trim().clone().to_string();
-        if let Ok(entries) = fs::read_dir(".") {
-            for entry in entries {
-                if let Ok(entry) = entry {
-                    println!("{:?}", entry.path());
-                }
-            }
-        }
-
-        let f = FileInfo::new(filename, "csv".to_string());
-        f.read_text_file();
-    }
+    let _ = match choice {
+        "c" => f.read_text_file(),
+        "b" => f.read_bytes(),
+        _ => (),
+    };
 }
