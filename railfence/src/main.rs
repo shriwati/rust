352785bbc7451f2de7cfcd97e2e4bacbae2b8654
shriwati <shrisakrikar@hotmail.com>
2024-12-03 08:00:19 -0500

    code optimized

--- railfence/src/main.rs
+++ railfence/src/main.rs
@@ -3,12 +3,12 @@ mod rail_fence;
 
 fn main() {
 
-    let msg=String::from("Shriisagoodboy!");
+    let msg=String::from("HELLO WORLD ");
     let encrypted_string:String=rail_fence::rail_fence::encrypt(&msg,3);
     println!("{}",&encrypted_string);
 
     println!("{}","");
-    println!("{:?}",rail_fence::rail_fence::decrypt(&encrypted_string,3));
+    println!("{}",rail_fence::rail_fence::decrypt(&encrypted_string,3));
 
 
 }
