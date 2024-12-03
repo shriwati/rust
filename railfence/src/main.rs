a555e3012e159a060bf98f43c2e5a7cbc491e817
shriwati <shrisakrikar@hotmail.com>
2024-12-03 08:18:24 -0500

    test functions added.

--- railfence/src/main.rs
+++ railfence/src/main.rs
@@ -3,6 +3,7 @@ mod rail_fence;
 
 fn main() {
 
+
     let msg=String::from("HELLO WORLD ");
     let encrypted_string:String=rail_fence::rail_fence::encrypt(&msg,3);
     println!("{}",&encrypted_string);
