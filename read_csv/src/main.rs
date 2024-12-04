568656f239454e1ad9320515cda5d7191925f98f
shriwati <shrisakrikar@hotmail.com>
2024-12-03 20:54:21 -0500

    test method added.

--- read_csv/src/main.rs
+++ read_csv/src/main.rs
@@ -5,13 +5,10 @@ mod read_csv;
 
 
 fn main() {
-    let relative_path = "/Users/shrisakrikar/Documents/data/boston_house.csv";
+    let relative_path = "/Users/shrisakrikar/Documents/data/pp-complete.csv";
     let reader = read_csv::read_file(relative_path);
     for line in reader{
         println!("{:?}",line);
     }
-    // if let Ok(line) = reader {
-    //     println!("{:?}",line);
-    // }
-    //
+
 }
--- read_csv/src/read_csv.rs
+++ read_csv/src/read_csv.rs
@@ -1,15 +1,9 @@
 
-
-
     use std::fs;
     use std::fs::File;
     use std::io::{BufRead, BufReader};
     use std::path::Path;
-/*
-    Open file (read-only)
-    Return Vector<String>
 
- */
 
     pub fn read_file(filename_with_path: &str) -> std::io::Result<Vec<String>> {
         //get absolute path
@@ -23,4 +17,13 @@
         let path = Path::new(path_str);
         let absolute_path = fs::canonicalize(path)?; // Resolve the absolute path
         Ok(absolute_path.to_string_lossy().to_string()) // Convert to string
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        #[test]
+        fn test_read_file() {
+
+        }
     }
