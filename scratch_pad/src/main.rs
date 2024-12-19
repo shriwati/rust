51d17911e66df362c6d8575164169710f11c6258
shriwati <shrisakrikar@hotmail.com>
2024-12-18 19:37:12 -0500

    input from command line

--- scratch_pad/src/main.rs
+++ scratch_pad/src/main.rs
@@ -1,3 +1,21 @@
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut input = String::new();
+
+    loop {
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        if &input.trim().to_lowercase() == "quit" {
+            println!("{}", input);
+            exit(1);
+        }
+        else {
+            println!("{}", input);
+            input.clear();
+        }
+    }
+
+
 }
