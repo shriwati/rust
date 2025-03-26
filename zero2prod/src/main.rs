5e867ed32aa83645e2dd57055cc10344191b34b3
shriwati <shrisakrikar@hotmail.com>
2025-03-26 19:11:47 -0400

    project skeleton

--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, HttpRequest, Responder};
+use serde::Deserialize;
+
+
+async fn greet(req:HttpRequest) -> impl Responder {
+    let name = req.match_info()
+        .get("name")
+        .unwrap_or("World");
+        format!("Hello {}!", &name)
 }
+async fn index() -> impl Responder {
+    "Hello world!"
+}
+#[tokio::main]
+async fn main()->std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/app")
+            .route("/{name}", web::get().to(greet))
+            .route("/index", web::get().to(index)))
+    }).bind("127.0.0.1:8080")
+    .unwrap()
+    .run().await
+}
+
+
