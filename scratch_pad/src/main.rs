b15751a3ef94bd1523d4d996d232cc9f9f31e417
shriwati <shrisakrikar@hotmail.com>
2025-06-18 08:14:07 -0400

    async tokio

--- scratch_pad/src/main.rs
+++ scratch_pad/src/main.rs
@@ -1,21 +1,11 @@
-use std::process::exit;
+
 
 fn main() {
+    
+}
 
-    let mut input = String::new();
-
-    loop {
-        std::io::stdin().read_line(&mut input).unwrap();
-
-        if &input.trim().to_lowercase() == "quit" {
-            println!("{}", input);
-            exit(1)
-        }
-        else {
-            println!("{}", input);
-            input.clear();
-        }
-    }
-
-
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {}
 }
