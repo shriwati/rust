463313e9fb76f7d4881f3ef99eceffc6f927e42e
shriwati <shrisakrikar@hotmail.com>
2025-12-01 21:35:02 -0500

    simple function print check-in

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,6 +1,17 @@
 pub mod print {
 
-    pub fn print() {
-        println!("Hello, world!");
+    pub fn world() ->String {
+        String::from("Hello, World!")
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::print::world;
+
+    #[test]
+    fn world_returns_hello_world() {
+        assert_eq!(world(), "Hello, World!", "Expecting 'Hello, World!', found '{}'", world());
     }
 }
