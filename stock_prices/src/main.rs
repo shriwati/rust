bd2c1e27f33f41c1ef8b4ae2a1ddab5959c05f12
shriwati <shrisakrikar@hotmail.com>
2026-01-27 14:57:43 -0500

    stock_prices code added.

---
+++ stock_prices/src/main.rs
@@ -0,0 +1,29 @@
+use log::{ info};
+use log4rs;
+use chrono::NaiveDate;
+use stock_prices::prices::{find_avg_price_for_dates, find_price_for_date};
+
+fn main()->Result<(),csv::Error> {
+        log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
+        info!("starting up");
+        let file_name = "/Users/shrisakrikar/Documents/data/AAPL.csv";
+        // get absolute path
+        let stock_prices= stock_prices::read_price(&file_name)?;
+        let avg_price = find_avg_price_for_dates(&stock_prices
+                                                 ,NaiveDate::from_ymd_opt(2025,1,1).unwrap()
+                                                 ,NaiveDate::from_ymd_opt(2025,12,31).unwrap());
+
+        let stock_price = find_price_for_date(&stock_prices,NaiveDate::from_ymd_opt(2025,12,31).unwrap());
+
+        match stock_price {
+                Some(stock_price) => println!("Price for Apple is {:.4} ", stock_price.close_price),
+                None => println!("Price did not receive: ", ),
+        }
+
+        match avg_price {
+                Some(price) => println!("Avg price for Apple is {:.4} ", price),
+                None => println!("Average price did not receive: ", ),
+        }
+        Ok(())
+    }
+
