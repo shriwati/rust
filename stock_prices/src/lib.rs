32f4ceef7f6f843b3c20758330b509d6726e4423
shriwati <shrisakrikar@hotmail.com>
2026-01-27 22:13:00 -0500

    date_format_serde added

--- stock_prices/src/lib.rs
+++ stock_prices/src/lib.rs
@@ -1,38 +1,14 @@
-use crate::prices::StockPrices;
-use csv::StringRecord;
-use log::{error, info};
-pub mod date_format {
-    use chrono::NaiveDate;
-    use serde::{self, Deserialize, Deserializer, Serializer};
-
-    const FORMAT: &str = "%m/%d/%y";
-
-    pub fn serialize<S>(date: &NaiveDate, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let s = date.format(FORMAT).to_string();
-        serializer.serialize_str(&s)
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDate, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        NaiveDate::parse_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
-    }
-}
-
 pub mod prices {
     use chrono::NaiveDate;
+    use csv::StringRecord;
     use serde::{Deserialize, Serialize};
-    use crate::date_format;
+    use date_format_serde::date_format_naive;
+    use log::{error, info};
 
     #[derive(Debug, PartialEq, Serialize, Deserialize)]
     pub struct StockPrices {
         name: String,
-        #[serde(with = "date_format")]
+        #[serde(with = "date_format_naive")]
         pub trade_date: NaiveDate,
         pub open_price: f64,
         pub high_price: f64,
@@ -81,60 +57,92 @@ pub mod prices {
             }
     }
 
-    // Serialize and deserialize dates in CSV file
+    pub fn read_price(file_name: &str) ->Result<Vec<StockPrices>,csv::Error> {
+        info!("reading csv file: {}", file_name);
+        // log every step of the way
+        // Open the file with full path
+        let mut stock_prices: Vec<StockPrices> = Vec::new();
+        let header = StringRecord::from(vec![
+            "name", "trade_date", "open_price", "high_price", "low_price", "close_price", "volume",
+        ]);
+        // get a reader
+        // check if file exists
+        if !std::path::Path::new(file_name).exists() {
+            return Err(csv::Error::from(std::io::Error::new(std::io::ErrorKind::NotFound, "file not found")));
+        }
+        let mut rdr = csv::Reader::from_path(file_name);
+        // if OK
+        match &mut rdr {
+            Ok(rdr) => {
+                // for each record in reader
+                for record in rdr.records() {
+                    match record {
+                        Ok(record) => {
+                            info!("Deserializing record: {:?}", record);
+                            match record.deserialize(Some(&header)) {
+                                Ok(stock_price) => {
+                                    // if reader is OK
+                                    info!("Deserialized record: {:?}", &stock_price);
+                                    // populate the StockPrices struct and push it to the vector
+                                    stock_prices.push(stock_price);
+                                }
 
-}
-
-pub fn read_price(file_name: &str) ->Result<Vec<StockPrices>,csv::Error> {
-    info!("reading csv file: {}", file_name);
-    // log every step of the way
-    // Open the file with full path
-    let mut stock_prices: Vec<StockPrices> = Vec::new();
-    let header = StringRecord::from(vec![
-        "name", "trade_date", "open_price", "high_price", "low_price", "close_price", "volume",
-    ]);
-    // get a reader
-    // check if file exists
-    if !std::path::Path::new(file_name).exists() {
-        return Err(csv::Error::from(std::io::Error::new(std::io::ErrorKind::NotFound, "file not found")));
-    }
-    let mut rdr = csv::Reader::from_path(file_name);
-    // if OK
-    match &mut rdr {
-        Ok(rdr) => {
-            // for each record in reader
-            for record in rdr.records() {
-                match record {
-                    Ok(record) => {
-                        info!("Deserializing record: {:?}", record);
-                        match record.deserialize(Some(&header)) {
-                            Ok(stock_price) => {
-                                // if reader is OK
-                                info!("Deserialized record: {:?}", &stock_price);
-                                // populate the StockPrices struct and push it to the vector
-                                stock_prices.push(stock_price);
+                                Err(e) => error!("error deserializing record: {:?} - {} ", record, e),
                             }
-
-                            Err(e) => error!("error deserializing record: {:?} - {} ", record, e),
                         }
-                    }
-                    Err(e) => {
-                        error!("error parsing CSV record: {}", e);
+                        Err(e) => {
+                            error!("error parsing CSV record: {}", e);
+                        }
                     }
                 }
             }
+            // else report file not found
+            Err(e) => {
+                error!("error reading csv file: {} - error {}", &file_name, e);
+            }
         }
-        // else report file not found
-        Err(e) => {
-            error!("error reading csv file: {} - error {}", &file_name, e);
-        }
+        Ok(stock_prices)
     }
-    Ok(stock_prices)
+
+}
+
+#[cfg(test)]
+mod test{
+    use chrono::NaiveDate;
+    use crate::prices::{read_price,find_avg_price_for_dates,find_price_for_date};
+    #[test]
+    fn test_read_price() {
+        let file_name = "/Users/shrisakrikar/Documents/data/AAPL.csv";
+        let stock_prices = read_price(&file_name).unwrap();
+        assert!(!stock_prices.is_empty());
+    }
+    #[test]
+    fn find_avg_price_for_date(){
+        let file_name = "/Users/shrisakrikar/Documents/data/AAPL.csv";
+        let stock_prices = read_price(&file_name).unwrap();
+        let from_date = NaiveDate::from_ymd_opt(2025,1,1).unwrap();
+        let to_date = NaiveDate::from_ymd_opt(2025,3,31).unwrap();
+        let price = find_avg_price_for_dates(&stock_prices, from_date,to_date );
+        assert!(price.is_some());
+    }
+    #[test]
+    fn no_price(){
+        let file_name = "/Users/shrisakrikar/Documents/data/AAPL.csv";
+        let stock_prices = read_price(&file_name).unwrap();
+        let from_date = NaiveDate::from_ymd_opt(2025,1,1).unwrap();
+        let price = find_price_for_date(&stock_prices,from_date);
+        assert!(price.is_none());
+    }
+
+    #[test]
+    fn find_price(){
+        let file_name = "/Users/shrisakrikar/Documents/data/AAPL.csv";
+        let stock_prices = read_price(&file_name).unwrap();
+        let from_date = NaiveDate::from_ymd_opt(2025,1,2).unwrap();
+        let price = find_price_for_date(&stock_prices,from_date);
+        assert!(price.unwrap().close_price == 243.85);
+    }
+
 }
 
 
-// #[cfg(test)]
-// mod tests {
-//
-//     #[test]
-// }
--- stock_prices/src/main.rs
+++ stock_prices/src/main.rs
@@ -1,14 +1,14 @@
 use log::{ info};
 use log4rs;
 use chrono::NaiveDate;
-use stock_prices::prices::{find_avg_price_for_dates, find_price_for_date};
+use stock_prices::prices::{find_avg_price_for_dates, find_price_for_date,read_price};
 
 fn main()->Result<(),csv::Error> {
         log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
         info!("starting up");
         let file_name = "/Users/shrisakrikar/Documents/data/AAPL.csv";
         // get absolute path
-        let stock_prices= stock_prices::read_price(&file_name)?;
+        let stock_prices= read_price(&file_name)?;
         let avg_price = find_avg_price_for_dates(&stock_prices
                                                  ,NaiveDate::from_ymd_opt(2025,1,1).unwrap()
                                                  ,NaiveDate::from_ymd_opt(2025,12,31).unwrap());
